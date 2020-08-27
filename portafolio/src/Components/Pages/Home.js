43646c4f19e2ae99f41d0468f0d60737565c8f6d
Jimmy P <jimepereda93@gmail.com>
2020-08-27 18:08:10 -0500

    final

--- portafolio/src/Components/Pages/Home.js
+++ portafolio/src/Components/Pages/Home.js
@@ -3,13 +3,12 @@ import homeImg from "../Images/mainImg.jpg"
 import './style.css';
 
 
-
 function Home() {
-  
-      return (
-        
+     
+      return ( 
+
        <div className="homebod">
-            <img src={homeImg} alt="" />
+            <img className="backG" src={homeImg} alt="" />
        </div>
     
       );
--- portafolio/src/Components/Pages/Portafolio/Portafolio.js
+++ portafolio/src/Components/Pages/Portafolio/Portafolio.js
@@ -1,54 +1,85 @@
 import React from "react";
 import {  Card, Button, CardGroup } from 'react-bootstrap';
 import './style.css';
+import Code from "./Images/CodeQuizz.jpg";
+import Siller from "./Images/Siller.jpg";
+import slot from "./Images/slot.jpg";
+import survive from "./Images/survive.jpg";
+import weather from "./Images/weather.jpg";
+import Coming from "./Images/Coming.jpg";
 
 
 
 function Portafolio(){
    return(
-    <div>
+    <div className="cardContainer">
          <CardGroup>
-  <Card className="cardCoint">
-    <Card.Img variant="top" src="holder.js/100px160" />
+  <Card>
+    <Card.Img variant="top" src={Siller} />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
+      <Card.Title>Grupo  <br/>Siller</Card.Title>
+      
     </Card.Body>
     <Card.Footer>
-    <Button variant="primary">Go somewhere</Button>
+    <Button variant="primary">Take me there!</Button>
     </Card.Footer>
   </Card>
-  <Card className="cardCoint">
-    <Card.Img variant="top" src="holder.js/100px160" />
+  <div className="divier" />
+  <Card>
+    <Card.Img variant="top" src={Code} />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
+      <Card.Title>Code <br/> Quizz </Card.Title>
+      
     </Card.Body>
     <Card.Footer>
-    <Button variant="primary">Go somewhere</Button>
+    <Button variant="primary">Take me there!</Button>
     </Card.Footer>
   </Card>
-  <Card className="cardCoint">
-    <Card.Img variant="top" src="holder.js/100px160" />
+  <div className="divier" />
+  <Card>
+    <Card.Img variant="top" src={weather}/>
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
+      <Card.Title>Weather <br/> Dashboard</Card.Title>
     </Card.Body>
     <Card.Footer>
-    <Button variant="primary">Go somewhere</Button>
+    <Button variant="primary">Take me there!</Button>
     </Card.Footer>
   </Card>
 </CardGroup>
+<CardGroup>
+<div className="divier" />
+  <Card>
+    <Card.Img variant="top" src={survive} />
+    <Card.Body>
+      <Card.Title>Survive  <br/>the Day</Card.Title>
+    </Card.Body>
+    <Card.Footer>
+    <Button variant="primary">Take me there!</Button>
+    </Card.Footer>
+  </Card>
+  <div className="divier" />
+  <Card>
+    <Card.Img variant="top" src={slot} />
+    <Card.Body>
+      <Card.Title>Slot  <br/> Machine</Card.Title>
+    
+    </Card.Body>
+    <Card.Footer>
+    <Button variant="primary">Take me there!</Button>
+    </Card.Footer>
+  </Card>
+  <div className="divier" />
+  <Card>
+    <Card.Img variant="top" src={Coming} />
+    <Card.Body>
+      <Card.Title>Coming <br/> soon...</Card.Title>
+    </Card.Body>
+    <Card.Footer>
+    <Button variant="primary">Nowhere to go</Button>
+    </Card.Footer>
+  </Card>
+</CardGroup>
+
     </div>
 
    )
